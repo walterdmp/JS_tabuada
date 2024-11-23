eb9d90bbc0ce5077b3c0b808e2c2964a85321a8e
Walter <wdiasmarquespereira@gmail.com>
2024-11-23 19:27:27 -0300

    Programa

---
+++ script.js
@@ -0,0 +1,14 @@
+function calcular(){
+    let numero = Number(document.getElementById('numero').value);
+    let resultado = document.getElementById('resultado');
+
+    if (numero > 0) {
+        let tabuada = '';
+        for (let i = 0; i <= 10; i++) {
+            tabuada += `${i} x ${numero} = ${i * numero}<br>`;
+        }
+        resultado.innerHTML = tabuada;
+    } else {
+        resultado.innerHTML = 'Por favor, insira um número positivo.';
+    }
+}
