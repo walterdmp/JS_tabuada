20eb1feb0857402852fe04e68d3ec63a4d253717
Walter <wdiasmarquespereira@gmail.com>
2024-11-23 19:35:03 -0300

    modificacao

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function calcular(){
 
     if (numero > 0) {
         let tabuada = '';
-        for (let i = 0; i <= 10; i++) {
+        for (let i = 1; i <= 10; i++) {
             tabuada += `${i} x ${numero} = ${i * numero}<br>`;
         }
         resultado.innerHTML = tabuada;
